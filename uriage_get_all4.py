9d843d48bbad6eaf8b4f90fea9247030f4cb0b5b
Kagumi Takekuma <80142118+kagumitakekuma@users.noreply.github.com>
2022-02-01 11:05:41 +0900

    Add files via upload

--- uriage_get_all4.py
+++ uriage_get_all4.py
@@ -3,10 +3,9 @@ import openpyxl as excel
 book =excel.load_workbook("uriage.xlsx",data_only=True)
 sheet =book.active
 
-rows =sheet["A3:F11"]
+rows =sheet["A3":"F9"]
 for row in rows:
     values =[cell.value for cell in row]
+    if values[0] is None:break
     print(values)
-
     
-            
