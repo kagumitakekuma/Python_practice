d33c47735cbe0066589d7cc233e7bcd1c6f016f8
Kagumi Takekuma <80142118+kagumitakekuma@users.noreply.github.com>
2022-02-08 14:34:33 +0900

    Add files via upload

---
+++ chrome_headless.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+import time
+
+options =webdriver.ChromeOptions()
+options.add_argument("--headless")
+driver =webdriver.Chrome(options=options)
+
+driver.get("https://uta.pw/sakusibbs/users.php?user_id=1")
+
+a_list =driver.find_elements_by_css_selector(
+    "ul#mmlist li a")
+
+for a in a_list:
+    print("■", a.text)
+    print("^", a.get_attribute("href"))
+
+driver.close()
