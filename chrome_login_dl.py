786f33aed388f71f8e3f452347733da0e4617352
Kagumi Takekuma <80142118+kagumitakekuma@users.noreply.github.com>
2022-02-09 14:33:17 +0900

    Add files via upload

---
+++ chrome_login_dl.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+import os, time
+
+login_url ="https://uta.pw/sakusibbs/users.php?action=login"
+user_id, password =("JS-TESTER","ipCU12ySxI")
+
+save_dir =os.path.dirname(os.path.abspath(__file__))
+save_file =save_dir +"/list.csv"
+
+options =webdriver.ChromeOptions()
+options.add_experimental_option("prefs",{
+    "dowonload.default_directory":save_dir})
+
+def login_download():
+    driver =webdriver.Chrome(options=options)
+    try_login(driver)
+    link_click(driver,"マイページ")
+    link_click(driver,"csvでダウンロード")
+
+    for i in range(30):
+              if os.path.exists(save_file):break
+              time.sleep(1)
+
+
+def try_login(driver):
+    driver.get(login_url)
+    usr =driver.find_element_by_name("usrrname_mmlbbs6")
+    usr.send_keys(user_id)
+    pwd =driver.find_element_by_name("password_mmlbbs6")
+    pwd.send_keys(password)
+    pwd.submit()
+
+
+def link_click(driver, label):
+    a =driver,find_element_by_partical_link_text(label)
+    a.click()
+
+if __name__ =="__main__":
+    login_download()
